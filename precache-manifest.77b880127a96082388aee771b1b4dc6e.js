e7e724c112b0233cab081739f7a95f987e5e6bef
wannasakon <wannasakon.ph.61@ubu.ac.th>
2020-09-30 11:47:36 +0700

    Updates

---
+++ precache-manifest.77b880127a96082388aee771b1b4dc6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94688df95189266bf4b3fd02ffc121a6",
+    "url": "/commath/index.html"
+  },
+  {
+    "revision": "7b5c2637e2e858e472f2",
+    "url": "/commath/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44efbeb1a345ba240663",
+    "url": "/commath/static/js/2.6b633340.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/commath/static/js/2.6b633340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b5c2637e2e858e472f2",
+    "url": "/commath/static/js/main.7a532828.chunk.js"
+  },
+  {
+    "revision": "0fae5acc01fe49813a9f",
+    "url": "/commath/static/js/runtime-main.04bf571c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/commath/static/media/logo.5d5d9eef.svg"
+  }
+]);
